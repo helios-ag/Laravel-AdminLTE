298acbf2758035306775f54aa565b3e68be6a313
Al Ganiev <helios.ag@gmail.com>
2016-11-01 01:52:34 +1000

    support russian language

---
+++ resources/lang/ru/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Полное имя',
+    'email'                       => 'Email',
+    'password'                    => 'Пароль',
+    'retype_password'             => 'Повторить пароль',
+    'remember_me'                 => 'Запомнить меня',
+    'register'                    => 'Регистрация',
+    'register_a_new_membership'   => 'Новая регистрация',
+    'i_forgot_my_password'        => 'Я забыл пароль',
+    'i_already_have_a_membership' => 'Я уже зарегистрирован',
+    'sign_in'                     => 'Вход',
+    'log_out'                     => 'Выйти',
+    'toggle_navigation'           => 'Включить навигацию',
+    'login_message'               => 'Войти',
+    'register_message'            => 'Новая регистрация',
+    'password_reset_message'      => 'Сброс пароля',
+    'reset_password'              => 'Сбросить пароль',
+    'send_password_reset_link'    => 'Отправить ссылку для восстановления пароля',
+];
